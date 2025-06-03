c94e0192c70bbe77f48bd2332839d7580d3da02a
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2025-06-03 13:09:28 -0400

    Add Python logging example

---
+++ Logging-Python/README.md
@@ -0,0 +1,3 @@
+This example code is adapted from
+<https://github.com/daboross/fern/blob/main/examples/pretty-colored.rs> for use
+in Python using [coloredlogs](https://pypi.org/project/coloredlogs).
---
+++ Logging-Python/main.py
@@ -0,0 +1,44 @@
+import logging
+import time
+
+import coloredlogs
+
+
+def set_up_logging(global_level: str = "DEBUG") -> None:
+    coloredlogs.install(level=global_level)
+
+
+def main() -> None:
+    set_up_logging("DEBUG")
+    logger = logger = logging.getLogger(__name__)
+
+    logger.info("starting simulation!")
+
+    for i in range(1, 101):
+        match i:
+            case 5:
+                logger.debug("this is taking so long... boooring!")
+            case 10:
+                logger.debug("still alive! yay!")
+            case 15:
+                logger.info("halfway there!")
+            case 20:
+                logger.debug("*scratches nose*")
+                logger.warning("nose is itching, continuing anyways")
+            case 50:
+                logger.debug("uh oh")
+                logger.warning(">nose itching intensifies")
+                logger.error("HATCHOOO!")
+                logger.debug("encountered minor problem, trying to recover")
+                logger.info("gesundheit")
+                logger.debug("recovered from minor problem, continuing")
+            case 100:
+                logger.info("successfully loaded nothing")
+                logger.info("have a good time!")
+
+        # Sleep for 100 milliseconds
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
